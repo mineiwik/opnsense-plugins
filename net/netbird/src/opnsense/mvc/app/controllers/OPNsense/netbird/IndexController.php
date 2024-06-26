9b25651b90acc6dcea82f0c9dce3a6203d1aa527
Ralph Moser <moser@pjm.co.at>
2024-06-26 14:47:07 +0200

    Removed comment

--- net/netbird/src/opnsense/mvc/app/controllers/OPNsense/netbird/IndexController.php
+++ net/netbird/src/opnsense/mvc/app/controllers/OPNsense/netbird/IndexController.php
@@ -10,8 +10,6 @@ class IndexController extends \OPNsense\Base\IndexController
 {
     public function indexAction()
     {
-        // pick the template to serve to our users.
-        // fetch form data "general" in
         $this->view->generalForm = $this->getForm("general");
         $this->view->pick('OPNsense/netbird/index');
     }
